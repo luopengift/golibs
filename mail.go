710c5c0d347c3080f5b397fcdde22de4e3066512
luopengift <870148195@qq.com>
2016-04-10 00:23:23 +0800

    Update mail.go

--- mail.go
+++ mail.go
@@ -1,19 +1,33 @@
 package golibs
 
 import (
-        "net/smtp"
-        "strings"
+    "net/smtp"
+    "strings"
 )
 
-func SendToMail(username, password, host, to, subject, body, mailtype string) error {
-        auth := smtp.PlainAuth("", username, password, strings.Split(host, ":")[0])
-        var contentType string
-        if mailtype == "html" {
-                contentType = "Content-Type: text/" + mailtype + "; charset=UTF-8"
-        } else {
-                contentType = "Content-Type: text/plain" + "; charset=UTF-8"
-        }
-        msg := []byte("To: " + to + "\r\n" + "From: " + username + ">\r\nSubject: " + "\r\n" + contentType + "\r\n\r\n" + body)
-        err := smtp.SendMail(host, auth, username, strings.Split(to, ";"), msg)
-        return err
+type SMTPServer struct {
+    Server string
+    Sender string
+    Passwd string
+}
+
+type Mail struct {
+    To      string
+    Type    string
+    Subject string
+    Body    string
+}
+
+func SendMail(server SMTPServer, mail Mail) error {
+    auth := smtp.PlainAuth("", server.Sender, server.Passwd, strings.Split(server.Server, ":")[0])
+    var Type string
+    switch mail.Type {
+    case "html":
+        Type = "Content-Type: text/" + mail.Type + "; charset=UTF-8"
+    default:
+        Type = "Content-Type: text/plain" + "; charset=UTF-8"
+    }
+    msg := []byte("To: " + mail.To + "\r\n" + "From: " + server.Sender + ">\r\n" + "Subject: " + "\r\n" + Type + "\r\n\r\n" + mail.Body)
+    err := smtp.SendMail(server.Server, auth, server.Sender, strings.Split(mail.To, ";"), msg)
+    return err
 }
