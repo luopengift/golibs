21d55483cc2e24be0f00fec74249b6ee68c4a8b9
luopengift <870148195@qq.com>
2017-03-31 16:00:51 +0800

    add crypto

---
+++ crypto/md5.go
@@ -0,0 +1,16 @@
+package crypto
+
+import (
+    "crypto/md5"
+    "encoding/hex"
+)
+
+//生成字符串的MD5值
+func MD5(str ...string) string {
+    h := md5.New()
+    for _, s := range str {
+        h.Write([]byte(s))
+    }
+    return hex.EncodeToString(h.Sum(nil))
+}
+
