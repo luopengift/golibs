6886c64903c774cfd7e6b41cf38c6f8a47e5269e
luopengift <870148195@qq.com>
2017-05-08 18:25:28 +0800

    新增kafka

---
+++ kafka/consumer_test.go
@@ -0,0 +1,18 @@
+package kafka
+
+import (
+    "testing"
+    "easyWork/framework/logs"
+)
+
+
+
+func Test_logs(t *testing.T) {
+    c := NewConsumer([]string{"172.31.4.53:9092"},"zhizi-monitor-data4",-1)
+    go func() {
+        for msg := range c.Message {
+            logs.Info(string(*msg))
+        }
+    }()
+    c.ReadFromTopic()
+}
