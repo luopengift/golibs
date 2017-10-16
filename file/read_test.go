112cba1d062372d531347ec7171aafc906bd21c0
luopengift <870148195@qq.com>
2017-10-16 16:02:26 +0800

    rm config.*

--- file/read_test.go
+++ file/read_test.go
@@ -15,42 +15,6 @@ func Test_read(t *testing.T) {
 	}
 }
 
-type RuntimeConfig struct {
-	DEBUG    bool `json:"DEBUG"`
-	MAXPROCS int  `json:"MAXPROCS"`
-}
-
-type KafkaConfig struct {
-	Addrs      []string `json:"addrs"`
-	Topic      string   `json:"topic"`
-	MaxThreads int64    `json:"maxthreads"`
-}
-
-type HttpConfig struct {
-	Addr string `json:"addr"`
-}
-
-type TestConfig struct {
-	Runtime RuntimeConfig
-	Kafka   KafkaConfig
-	File    []string `json:"file"`
-	Prefix  string   `json:"prefix"`
-	Suffix  string   `json:"suffix"`
-	Http    HttpConfig
-	Tags    string `json:"tags"`
-	Version string `json:version`
-}
-
-func Test_config(t *testing.T) {
-	test := &TestConfig{}
-	config := NewConfig("./config.json")
-	config.Parse(test)
-	fmt.Println(fmt.Sprintf("%+v", test))
-	fmt.Println(config)
-
-}
-
-
 func Test_tail(t *testing.T) {
     f := NewTail("/tmp/test/t_%Y%M%D%h%m.log",&TimeRuler{})
     f.ReadLine()
